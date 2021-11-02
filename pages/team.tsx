4ef40c788b454d05d66e06bafe59d64ec67067a0
Thusharke <thushar.ke25@gmail.com>
2021-11-02 13:38:21 +0530

    minor changes

--- pages/team.tsx
+++ pages/team.tsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { TeamCard } from '../src/components/TeamCard';
+import React from "react";
+import { TeamCard } from "../src/components/TeamCard";
 
 const people = [
   {
-    name: 'Srimanth Tenneti',
-    role: 'Community Lead & Machine Learning Lead',
+    name: "Srimanth Tenneti",
+    role: "Community Lead & Machine Learning Lead",
     imageUrl:
-      'https://drive.google.com/file/d/19OwoufmQHWrwPICqfvnEvJ0ApFKBXzz5/view',
-    bio: 'Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.',
-    twitterUrl: '#',
-    linkedinUrl: '#',
+      "https://drive.google.com/file/d/19OwoufmQHWrwPICqfvnEvJ0ApFKBXzz5/view",
+    bio: "Ultricies massa malesuada viverra cras lobortis. Tempor orci hac ligula dapibus mauris sit ut eu. Eget turpis urna maecenas cras. Nisl dictum.",
+    twitterUrl: "#",
+    linkedinUrl: "#",
   },
   // More people...
 ];
 
 const team: React.FC = () => {
   return (
-    <div className='bg-white'>
-      <div className='mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24'>
-        <div className='space-y-5 sm:space-y-4 mb-12'>
-          <h2 className='text-3xl font-extrabold tracking-tight sm:text-4xl'>
+    <div className="bg-white">
+      <div className="mx-auto py-12 px-4 max-w-7xl sm:px-6 lg:px-8 lg:py-24">
+        <div className="space-y-5 sm:space-y-4 mb-12">
+          <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
             Our Team
           </h2>
         </div>
-        <div className='space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0'>
-          <div className='lg:col-span-3'>
+        <div className="space-y-12 lg:grid lg:grid-cols-3 lg:gap-8 lg:space-y-0">
+          <div className="lg:col-span-3">
             <ul
-              role='list'
-              className='space-y-12 sm:grid sm:grid-cols-3 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:gap-x-8'
+              role="list"
+              className="space-y-12 sm:grid sm:grid-cols-3 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:gap-x-8"
             >
               {people.map((person) => (
                 <li key={person.name}>
