ff23b99d1dfcc5e7a17cb2faabc18b24d1a2029b
adilshaik <shaik.adil@hotmail.com>
2021-11-03 14:18:39 +0530

    Updated events

--- pages/events.tsx
+++ pages/events.tsx
@@ -4,9 +4,80 @@ import { EventCard } from '../src/components/EventCard';
 import { EventHero } from '../src/components/EventHero';
 import { Nav } from '../src/components/Layouts/Nav';
 
-const eventList = [];
+const eventList = [
+  {
+    title: 'GDSC CVR Kickstart Event',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+  temporibus dicta tempore, doloribus incidunt magni repudiandae id
+  perferendis. Quas, hic!`,
+    date: '15 Sept 2021',
+    time: '11:00 pm',
+    watchUrl: '',
+    registerUrl: '',
+  },
+  {
+    title: 'Introduction to 30 days of google cloud',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+  temporibus dicta tempore, doloribus incidunt magni repudiandae id
+  perferendis. Quas, hic!`,
+    date: '02 Nov 2021',
+    time: '6:00 pm',
+    watchUrl: '',
+    registerUrl: '',
+  },
+  {
+    title: 'Introduction to 30 days of google cloud',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+  temporibus dicta tempore, doloribus incidunt magni repudiandae id
+  perferendis. Quas, hic!`,
+    date: '02 Nov 2021',
+    time: '6:00 pm',
+    watchUrl: '',
+    registerUrl: '',
+  },
+  {
+    title: 'Introduction to 30 days of google cloud',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+  temporibus dicta tempore, doloribus incidunt magni repudiandae id
+  perferendis. Quas, hic!`,
+    date: '02 Nov 2021',
+    time: '6:00 pm',
+    watchUrl: '',
+    registerUrl: '',
+  },
+  {
+    title: 'Introduction to 30 days of google cloud',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+  temporibus dicta tempore, doloribus incidunt magni repudiandae id
+  perferendis. Quas, hic!`,
+    date: '02 Nov 2021',
+    time: '6:00 pm',
+    watchUrl: '',
+    registerUrl: '',
+  },
+  {
+    title: 'Introduction to 30 days of google cloud',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+  temporibus dicta tempore, doloribus incidunt magni repudiandae id
+  perferendis. Quas, hic!`,
+    date: '02 Nov 2021',
+    time: '6:00 pm',
+    watchUrl: '',
+    registerUrl: '',
+  },
+  {
+    title: 'Introduction to 30 days of google cloud',
+    description: `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
+  temporibus dicta tempore, doloribus incidunt magni repudiandae id
+  perferendis. Quas, hic!`,
+    date: '02 Nov 2021',
+    time: '6:00 pm',
+    watchUrl: '',
+    registerUrl: '',
+  },
+];
 
-const events = () => {
+const events: React.FC = () => {
   return (
     <React.Fragment>
       <Head>
@@ -16,7 +87,7 @@ const events = () => {
       <Nav />
       <main className='pt-24'>
         <EventHero />
-        <EventCard />
+        <EventCard content={eventList} />
       </main>
     </React.Fragment>
   );
--- src/components/EventCard.tsx
+++ src/components/EventCard.tsx
@@ -1,44 +1,72 @@
-import React, { useEffect, useState } from "react";
-import AOS from "aos";
-import "aos/dist/aos.css";
+import React, { useEffect, useState } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
-export const EventCard = () => {
+interface IProps {
+  content: IEventContent[];
+}
+
+interface IEventContent {
+  title: string;
+  description: string;
+  date: string;
+  time: string;
+  registerUrl: string;
+  watchUrl: string;
+}
+
+export const EventCard: React.FC<IProps> = (props) => {
   useEffect(() => {
-    AOS.init();
+    AOS.init({
+      delay: 4, // values from 0 to 3000, with step 50ms
+      duration: 600, // values from 0 to 3000, with step 50ms
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: false, // whether elements should animate out while scrolling past them
+    });
   }, []);
+
   return (
-    <div
-      data-aos="fade-left"
-      className="max-w-4xl mx-auto rounded-xl my-20 p-4 flex items-center shadow-xl"
-    >
-      <div
-        style={{ backgroundColor: "#ECEBFA" }}
-        className="w-52 h-32 rounded-md flex flex-col text-center items-center justify-center"
-      >
-        <h4 className="font-extrabold text-xl italic">02 Nov 2021</h4>
-        <h4 className="uppercase my-1 text-lg font-bold text-gray-600 tracking-wide">
-          6:00 pm
-        </h4>
-      </div>
-      <div className="w-full ml-4 p-2 flex flex-col">
-        <h3 className="text-xl font-semibold mb-2">
-          Introduction to 30 days of google cloud
-        </h3>
-        <p className="text-md font-semibold text-gray-500 pr-7">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere
-          temporibus dicta tempore, doloribus incidunt magni repudiandae id
-          perferendis. Quas, hic!
-        </p>
-        {new Date().getDate() > new Date("02 Nov 2021").getDate() ? (
-          <a href="" className="my-2 text-indigo-700 font-semibold underline">
-            Watch Video
-          </a>
-        ) : (
-          <a href="" className="my-2 text-indigo-700 font-semibold underline">
-            Register
-          </a>
-        )}
-      </div>
-    </div>
+    <React.Fragment>
+      {props &&
+        props.content.map((event) => {
+          return (
+            <div
+              data-aos='fade-left'
+              className='max-w-4xl mx-auto rounded-xl my-20 p-4 flex items-center shadow-xl'
+            >
+              <div
+                style={{ backgroundColor: '#ECEBFA' }}
+                className='w-52 h-32 rounded-md flex flex-col text-center items-center justify-center'
+              >
+                <h4 className='font-extrabold text-xl italic'>{event.date}</h4>
+                <h4 className='uppercase my-1 text-lg font-bold text-gray-600 tracking-wide'>
+                  {event.time}
+                </h4>
+              </div>
+              <div className='w-full ml-4 p-2 flex flex-col'>
+                <h3 className='text-xl font-semibold mb-2'>{event.title}</h3>
+                <p className='text-md font-semibold text-gray-500 pr-7'>
+                  {event.description}
+                </p>
+                {new Date().getDate() > new Date(event.date).getDate() ? (
+                  <a
+                    href=''
+                    className='my-2 text-indigo-700 font-semibold underline'
+                  >
+                    Watch Video
+                  </a>
+                ) : (
+                  <a
+                    href={event.registerUrl}
+                    className='my-2 text-indigo-700 font-semibold underline'
+                  >
+                    Register
+                  </a>
+                )}
+              </div>
+            </div>
+          );
+        })}
+    </React.Fragment>
   );
 };
