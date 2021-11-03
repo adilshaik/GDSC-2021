f9e2535a7d9287dbda8c8f8102e31c4450b380bb
Thusharke <thushar.ke25@gmail.com>
2021-11-03 14:16:33 +0530

    added tracks page

--- pages/events.tsx
+++ pages/events.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head';
-import React from 'react';
-import { EventCard } from '../src/components/EventCard';
-import { EventHero } from '../src/components/EventHero';
-import { Nav } from '../src/components/Layouts/Nav';
+import Head from "next/head";
+import React from "react";
+import { EventCard } from "../src/components/EventCard";
+import { EventHero } from "../src/components/EventHero";
+import { Nav } from "../src/components/Layouts/Nav";
 
 const eventList = [];
 
@@ -11,10 +11,10 @@ const events = () => {
     <React.Fragment>
       <Head>
         <title>GDSC</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Nav />
-      <main className='pt-24'>
+      <main className="pt-24">
         <EventHero />
         <EventCard />
       </main>
--- pages/tracks.tsx
+++ pages/tracks.tsx
@@ -1,31 +1,34 @@
 import React from "react";
 import {
   NewspaperIcon,
-  PhoneIcon,
-  SupportIcon,
+  DeviceMobileIcon,
+  EyeIcon,
+  GlobeAltIcon,
 } from "@heroicons/react/outline";
-
+import { TrackCard } from "../src/components/TrackCard";
+import { Nav } from "../src/components/Layouts/Nav";
+import { Footer } from "../src/components/Layouts/Footer";
 const supportLinks = [
   {
-    name: "Sales",
+    name: "Web Development",
     href: "#",
     description:
-      "Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.",
-    icon: PhoneIcon,
+      "Go from no-code to an in-demand web developer. Start with the front-end by learning HTML, CSS, and JavaScript. Then, master the back-end and APIs to round out your full-stack skills",
+    icon: GlobeAltIcon,
   },
   {
-    name: "Technical Support",
+    name: "Android Development",
     href: "#",
     description:
-      "Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.",
-    icon: SupportIcon,
+      "Learn to build mobile apps with Flutter. The use of mobile devices is increasing every year. Meet the operating system that powers nearly 90% of these device",
+    icon: DeviceMobileIcon,
   },
   {
-    name: "Media Inquiries",
+    name: "Machine Learning",
     href: "#",
     description:
-      "Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate id malesuada non. Cras aliquet purus dui laoreet diam sed lacus, fames.",
-    icon: NewspaperIcon,
+      "Whether you’re just learning to code or you’re a seasoned machine learning practitioner, you’ll find information and exercises to help you develop your skills and advance your projects.",
+    icon: EyeIcon,
   },
 ];
 
@@ -33,7 +36,8 @@ const tracks: React.FC = () => {
   return (
     <div className="bg-white">
       {/* Header */}
-      <div className="relative pb-32 bg-gray-800">
+      <Nav />
+      <div className="relative pb-32 bg-gray-800 pt-40">
         <div className="absolute inset-0">
           <img
             className="w-full h-full object-cover"
@@ -47,7 +51,7 @@ const tracks: React.FC = () => {
         </div>
         <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
           <h1 className="text-4xl font-extrabold tracking-tight text-white md:text-5xl lg:text-6xl">
-            Support
+            Tracks
           </h1>
           <p className="mt-6 max-w-3xl text-xl text-gray-300">
             Varius facilisi mauris sed sit. Non sed et duis dui leo, vulputate
@@ -62,41 +66,13 @@ const tracks: React.FC = () => {
         className="-mt-32 max-w-7xl mx-auto relative z-10 pb-32 px-4 sm:px-6 lg:px-8"
         aria-labelledby="contact-heading"
       >
-        <h2 className="sr-only" id="contact-heading">
-          Contact us
-        </h2>
         <div className="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8">
           {supportLinks.map((link) => (
-            <div
-              key={link.name}
-              className="flex flex-col bg-white rounded-2xl shadow-xl"
-            >
-              <div className="flex-1 relative pt-16 px-6 pb-8 md:px-8">
-                <div className="absolute top-0 p-5 inline-block bg-indigo-600 rounded-xl shadow-lg transform -translate-y-1/2">
-                  <link.icon
-                    className="h-6 w-6 text-white"
-                    aria-hidden="true"
-                  />
-                </div>
-                <h3 className="text-xl font-medium text-gray-900">
-                  {link.name}
-                </h3>
-                <p className="mt-4 text-base text-gray-500">
-                  {link.description}
-                </p>
-              </div>
-              <div className="p-6 bg-gray-50 rounded-bl-2xl rounded-br-2xl md:px-8">
-                <a
-                  href={link.href}
-                  className="text-base font-medium text-indigo-700 hover:text-indigo-600"
-                >
-                  Contact us<span aria-hidden="true"> &rarr;</span>
-                </a>
-              </div>
-            </div>
+            <TrackCard link={link} />
           ))}
         </div>
       </section>
+      <Footer />
     </div>
   );
 };
